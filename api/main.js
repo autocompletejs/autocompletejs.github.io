b97f212ae5941997004fe2c518763ece6d420c45
Baptiste Donaux <baptiste.donaux@gmail.com>
2016-07-28 09:51:28 +0200

    [API] rewrite code from PHP to Node.js

---
+++ api/main.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const countries = require("./countries.json");
+const app = require('express')()
+ 
+app.get('/', function (req, res) {
+	let query = "";
+	let returns = [];
+
+	if (req.query.q !== undefined) {
+		query = req.query.q;
+	}
+
+	for (let abr in countries) {
+		let country = countries[abr];
+
+		if (abr.match(new RegExp(query, "i")) || country.match(new RegExp(query, "i"))) {
+			returns.push(country);
+		}
+	}
+
+	res.send(JSON.stringify(returns));
+});
+ 
+app.listen(8000)
