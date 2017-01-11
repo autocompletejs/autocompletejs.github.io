42cd72e33c0901a92235e9c67404fe1cd3260fa4
Baptiste Donaux <baptiste.donaux@gmail.com>
2017-01-11 11:10:30 +0100

    [TYPO]

--- api/main.js
+++ api/main.js
@@ -4,23 +4,23 @@ const countries = require("./countries.json");
 const app = require("express")()
  
 app.get("/", function (req, res) {
-	let query = "";
-	let returns = [];
+    let query = "";
+    let returns = [];
 
-	if (req.query.q !== undefined) {
-		query = req.query.q;
-	}
+    if (req.query.q !== undefined) {
+        query = req.query.q;
+    }
 
-	for (let abr in countries) {
-		let country = countries[abr];
+    for (let abr in countries) {
+        let country = countries[abr];
 
-		if (abr.match(new RegExp(query, "i")) || country.match(new RegExp(query, "i"))) {
-			returns.push(country);
-		}
-	}
+        if (abr.match(new RegExp(query, "i")) || country.match(new RegExp(query, "i"))) {
+            returns.push(country);
+        }
+    }
 
-	res.set("Content-Type", "application/json");
-	res.send(JSON.stringify(returns));
+    res.set("Content-Type", "application/json");
+    res.send(JSON.stringify(returns));
 });
  
 app.listen(8000)
